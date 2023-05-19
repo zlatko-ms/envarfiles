2e377749f0d0b8ece5913c5eb7012646af0c1a74
zlatko-ms <zlatko@DESKTOP-OCMJV74>
2023-05-19 11:08:29 +0200

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,22 +3,21 @@
 varfile=$1
 output="["
 
+echo "definitions=none"
 
-echo "ZDBG starting import process"
+# if [ -z "$varfile" ] ; then
+#     echo "ERROR : missing varfile parameter"
+# else:
+#     while read p; do
+#             k=$(echo $p | sed s'/[ ]*=[ ]*/=/g')
+#             n=$(echo $k | cut -f1 -d'=')
+#             v=$(echo $k | cut -f2 -d'=')
+#             output="$output $n=$v"
+#             echo "$k" >> $GITHUB_ENV
+#             echo "ZDBG adding definition $k"
 
-if [ -z "$varfile" ] ; then
-    echo "ERROR : missing varfile parameter"
-else:
-    while read p; do
-            k=$(echo $p | sed s'/[ ]*=[ ]*/=/g')
-            n=$(echo $k | cut -f1 -d'=')
-            v=$(echo $k | cut -f2 -d'=')
-            output="$output $n=$v"
-            echo "$k" >> $GITHUB_ENV
-            echo "ZDBG adding definition $k"
-
-    done < $varfile
-    echo "definitions=$output" >> $GITHUB_OUTPUT
-fi
+#     done < $varfile
+#     echo "definitions=$output" >> $GITHUB_OUTPUT
+# fi
 
 
