ee4b1b7d05d3a9aa53773b9951f54a67ae8428ce
zlatko-ms <zlatko@DESKTOP-OCMJV74>
2023-05-19 11:18:36 +0200

    zdbg

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ output="["
 echo "[ZDBG] definining test value myvalye=ZDBGValue" >> $GITHUB_ENV
 echo "[ZDBG] processing file $varfile"
 
-while read p: do
+while read p; do
     k=$(echo $p | sed s'/[ ]*=[ ]*/=/g')
     echo "[ZDBG] found new var def $k"
 done < "$varfile"
