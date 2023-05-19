a50c27c77af9734fbea7b562658e3bcc4b479477
zlatko-ms <zlatko@DESKTOP-OCMJV74>
2023-05-19 11:25:11 +0200

    zdbg

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,38 +1,22 @@
 #!/bin/sh -l
 
 varfile=$1
-output="["
+output="from $varfile : "
 
-echo "[ZDBG] definining test value myvalye=ZDBGValue"
 echo "myvalue=ZDBGValue" >> $GITHUB_ENV
-echo "[ZDBG] processing file $varfile"
 
-while read p; do
-    k=$(echo $p | sed s'/[ ]*=[ ]*/=/g')
-    echo "[ZDBG] found new var def $k"
-    n=$(echo $k | cut -f1 -d'=')
-    v=$(echo $k | cut -f2 -d'=')
-    echo "[ZDBG] found new var $n=$v"
-    echo "$k" >> $GITHUB_ENV
-done < "$varfile"
+if [ -z "$varfile" ] ; then
+    echo "ERROR : missing varfile parameter"
+else:
+    while read p; do
+        k=$(echo $p | sed s'/[ ]*=[ ]*/=/g')
+        n=$(echo $k | cut -f1 -d'=')
+        v=$(echo $k | cut -f2 -d'=')
+        echo "$k" >> $GITHUB_ENV
+        output="$output $k"
+    done < "$varfile"
+fi
+echo "definitions=$output" >> >> $GITHUB_OUTPUT
 
-
-
-# if [ -z "$varfile" ] ; then
-#     echo "ERROR : missing varfile parameter"
-# else:
-#     while read p; do
-#             k=$(echo $p | sed s'/[ ]*=[ ]*/=/g')
-#             n=$(echo $k | cut -f1 -d'=')
-#             v=$(echo $k | cut -f2 -d'=')
-#             output="$output $n=$v"
-#             echo "$k" >> $GITHUB_ENV
-#             echo "ZDBG adding definition $k"
-
-#     done < $varfile
-#     echo "definitions=$output" >> $GITHUB_OUTPUT
-# fi
-
-#echo "definitions=none from varfile $varfile"
 
 
