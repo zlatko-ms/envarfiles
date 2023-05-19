3b28d1d69940c436684d214cb6be542824d95076
zlatko-ms <zlatko@DESKTOP-OCMJV74>
2023-05-19 15:28:41 +0200

    zdbg

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,6 @@
 varfile=$1
 output=""
 
-#echo "[ZDBG] definining test value myvalye=ZDBGValue"
-#echo "myvalue=ZDBGValue" >> $GITHUB_ENV
-#echo "[ZDBG] processing file $varfile"
-
 if [ "x$varfile" == "x" ] ; then
     echo "ERROR : no input specified"
     exit 255
@@ -23,21 +19,5 @@ done < "$varfile"
 echo "definitions=$output" >> $GITHUB_OUTPUT
 
 
-# if [ -z "$varfile" ] ; then
-#     echo "ERROR : missing varfile parameter"
-# else:
-#     while read p; do
-#             k=$(echo $p | sed s'/[ ]*=[ ]*/=/g')
-#             n=$(echo $k | cut -f1 -d'=')
-#             v=$(echo $k | cut -f2 -d'=')
-#             output="$output $n=$v"
-#             echo "$k" >> $GITHUB_ENV
-#             echo "ZDBG adding definition $k"
-
-#     done < $varfile
-#     echo "definitions=$output" >> $GITHUB_OUTPUT
-# fi
-
-#echo "definitions=none from varfile $varfile"
 
 
