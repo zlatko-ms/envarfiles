c77f1d953c2d6adca97ace78641056ed4f7167f3
zlatko-ms <zlatko@DESKTOP-OCMJV74>
2023-05-22 09:27:57 +0200

    testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,28 @@
 #!/bin/sh -l
 
-varfile=$1
+paths=$1
 output=""
 
-if [ "x$varfile" == "x" ] ; then
-    echo "ERROR : no input specified"
-    exit 255
-fi
+echo "[ZDBG] paths=$paths"
 
-output="$varfile:["
-while read p; do
-    k=$(echo $p | sed s'/[ ]*=[ ]*/=/g')
-    n=$(echo $k | cut -f1 -d'=')
-    v=$(echo $k | cut -f2 -d'=')
-    output="$output $k"
-    echo "$k" >> $GITHUB_ENV
-done < "$varfile"
-output="$output]"
-echo "definitions=$output" >> $GITHUB_OUTPUT
+
+#if [ "x$varfile" == "x" ] ; then
+#    echo "ERROR : no input specified"
+#    exit 255
+#fi
+
+
+
+# output="$varfile:["
+# while read p; do
+#     k=$(echo $p | sed s'/[ ]*=[ ]*/=/g')
+#     n=$(echo $k | cut -f1 -d'=')
+#     v=$(echo $k | cut -f2 -d'=')
+#     output="$output $k"
+#     echo "$k" >> $GITHUB_ENV
+# done < "$varfile"
+# output="$output]"
+# echo "definitions=$output" >> $GITHUB_OUTPUT
 
 
 
