23579eca1724a665e78499b9aa3856f38e6f37d8
zlatko-ms <zlatko@DESKTOP-OCMJV74>
2023-05-22 10:16:58 +0200

    testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,16 +4,20 @@ paths=$1
 output=""
 
 function logMessage() {
+
     levelIn=$1
     message=$2
     level=`echo "$levelIn" | tr '[:lower:]' '[:upper:]'`
     now=`date +%Y%m%d-%H%M%S`
     echo "[$now] [$level] $message"
+
 }
 
 function readVariablesFiles() {
 
     varfile=$1
+    outputRet="$varfile=["
+
     logMessage "info" "reading variable definitions from $varfile"
 
     while read p; do
@@ -21,31 +25,26 @@ function readVariablesFiles() {
         n=$(echo $k | cut -f1 -d'=')
         v=$(echo $k | cut -f2 -d'=')
         eval current="\$$n"
-        logMessage "debug" "current var $n value is $current"
-    #     echo "[ZDBG] current value of $n is ${!$n}"
-    #     echo "[ZDBG] file value of $n is $v"
-    #     #output=" $output $k"
+        #logMessage "debug" "current var $n value is $current"
+        outputRet="$outputRet $k"
         echo "$k" >> $GITHUB_ENV
     done < "$varfile"
-
-    #output="$output ]"
-
+    outputRet="$outputRet ]"
+    echo "$outputRet"
 }
 
 
-logMessage "INFO" "Starting variable definition processing"
-
-
-# let's parse all the provided files
 for varfile in $paths; do
     if [ -f "$varfile" ] ; then 
-        readVariablesFiles "$varfile"
+        retCall=$(readVariablesFiles "$varfile")
+        output="$output $retCall"
     else
         logMessage "warn" "ignoring file $varfile as it cannot be found"
     fi
 
 done
 
+echo "definitions=$output" >> $GITHUB_OUTPUT
 
 #if [ "x$varfile" == "x" ] ; then
 #    echo "ERROR : no input specified"
