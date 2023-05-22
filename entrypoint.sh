bbc8d8bc4c414a6f1bb5a4b548f8c53c1bfe53af
zlatko-ms <zlatko@DESKTOP-OCMJV74>
2023-05-22 10:55:15 +0200

    testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,27 +4,22 @@ paths=$1
 output="["
 
 function logMessage() {
-
     levelIn=$1
     message=$2
     level=`echo "$levelIn" | tr '[:lower:]' '[:upper:]'`
     now=`date +%Y%m%d-%H%M%S`
     echo "[$now] [$level] $message"
-
 }
 
 function readVariablesFiles() {
-
     varfile=$1
     outputRet="$varfile=["
-
     logMessage "info" "reading variable definitions from $varfile"
-
     while read p; do
         k=$(echo $p | sed s'/[ ]*=[ ]*/=/g')
         n=$(echo $k | cut -f1 -d'=')
         v=$(echo $k | cut -f2 -d'=')
-        eval current="\$$n"
+        #eval current="\$$n"
         #logMessage "debug" "current var $n value is $current"
         outputRet="$outputRet $k"
         logMessage "info" "found variable definition $k"
@@ -33,11 +28,6 @@ function readVariablesFiles() {
 
 }
 
-# if [ "x$paths" == "x" ] ; then
-#     logMessage "fatal" "no input files specified !"
-#     exit 255
-# fi
-
 for varfile in $paths; do
     if [ -f "$varfile" ] ; then 
         output="$output $varfile"
@@ -45,12 +35,14 @@ for varfile in $paths; do
     else
         logMessage "warn" "ignoring file $varfile as it cannot be found"
     fi
-
 done
 
 echo "processed=$output ]" >> $GITHUB_OUTPUT
 
-
+# if [ "x$paths" == "x" ] ; then
+#     logMessage "fatal" "no input files specified !"
+#     exit 255
+# fi
 
 
 
