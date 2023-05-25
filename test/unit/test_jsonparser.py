096157f35e9d641828d61f3bf4a16ea163c21f8b
zlatko-ms <zlatko@DESKTOP-OCMJV74>
2023-05-25 18:43:22 +0200

    fixing some messages in the unit tests, added fixtures for it on yaml

--- test/unit/test_jsonparser.py
+++ test/unit/test_jsonparser.py
@@ -8,9 +8,9 @@ class TestJsonParser(unittest.TestCase):
     nestedFile = "test/fixtures/unit/nested.json"
 
     def test_001_file_pattern_accetance(self) -> None:
-        self.assertTrue(JsonFileParser.accepts("/no/path/file.json"), ".json extensions are supported")
-        self.assertFalse(JsonFileParser.accepts("/no/path/file"), "no extensions are not supported")
-        self.assertFalse(JsonFileParser.accepts("/no/path/file.txt"), "only json extensions are not supported")
+        self.assertTrue(JsonFileParser.accepts("/no/path/file.json"), ".json extensions is supported")
+        self.assertFalse(JsonFileParser.accepts("/no/path/file"), "files without extensions are not supported")
+        self.assertFalse(JsonFileParser.accepts("/no/path/file.txt"), "only json extension is supported")
 
     def test_002_simple_file(self) -> None:
         props: dict = JsonFileParser.getVariablesDict(self.simpleFile)
@@ -27,7 +27,7 @@ class TestJsonParser(unittest.TestCase):
         self.assertEqual(props["build_version_major"], "1", "variable build_version_major has the expected value")
         self.assertEqual(props["build_version_minor"], "2", "variable build_version_minor has the expected value")
 
-    def test_003_nested_file_custom_se(self) -> None:
+    def test_004_nested_file_custom_se(self) -> None:
         props: dict = JsonFileParser.getVariablesDict(self.nestedFile, ".")
         self.assertEqual(len(props.keys()), 2, "nested file has 2 variables")
         self.assertTrue("build.version.minor" in props.keys(), "nested contains variable build.version.minor")
--- test/unit/test_yamlparser.py
+++ test/unit/test_yamlparser.py
@@ -8,12 +8,12 @@ class TestYAMLParser(unittest.TestCase):
     nestedFile = "test/fixtures/unit/nested.yaml"
 
     def test_001_file_pattern_accetance(self) -> None:
-        self.assertTrue(YamlFileParser.accepts("/no/path/file.yaml"), ".yaml extensions are supported")
-        self.assertTrue(YamlFileParser.accepts("/no/path/file.yml"), ".yml extensions are supported")
-        self.assertFalse(YamlFileParser.accepts("/no/path/file"), "no extensions are not supported")
-        self.assertFalse(YamlFileParser.accepts("/no/path/file.txt"), "only yaml and yml extensions are not supported")
+        self.assertTrue(YamlFileParser.accepts("/no/path/file.yaml"), ".yaml extension is supported")
+        self.assertTrue(YamlFileParser.accepts("/no/path/file.yml"), ".yml extension is supported")
+        self.assertFalse(YamlFileParser.accepts("/no/path/file"), "files without extensions are not supported")
+        self.assertFalse(YamlFileParser.accepts("/no/path/file.txt"), "only yaml and yml extensions are supported")
 
-        # def test_002_simple_file(self) -> None:
+    def test_002_simple_file(self) -> None:
         props: dict = YamlFileParser.getVariablesDict(self.simpleFile)
         self.assertEqual(len(props.keys()), 2, "simple file has 2 variables")
         self.assertTrue("major" in props.keys(), "simple contains variable major")
@@ -28,7 +28,7 @@ class TestYAMLParser(unittest.TestCase):
         self.assertEqual(props["build_version_major"], "1", "variable build_version_major has the expected value")
         self.assertEqual(props["build_version_minor"], "2", "variable build_version_minor has the expected value")
 
-    def test_003_nested_file_custom_se(self) -> None:
+    def test_004_nested_file_custom_se(self) -> None:
         props: dict = YamlFileParser.getVariablesDict(self.nestedFile, ".")
         self.assertEqual(len(props.keys()), 2, "nested file has 2 variables")
         self.assertTrue("build.version.minor" in props.keys(), "nested contains variable build.version.minor")
