83589aec9918f177512d1e9a5ecca80e35c3ff59
zlatko-ms <zlatko@DESKTOP-OCMJV74>
2023-05-25 18:31:58 +0200

    full unit test for yaml parsing

--- test/unit/test_yamlparser.py
+++ test/unit/test_yamlparser.py
@@ -3,7 +3,7 @@ import unittest
 from processor import YamlFileParser
 
 
-class TestJsonParser(unittest.TestCase):
+class TestYAMLParser(unittest.TestCase):
     simpleFile = "test/fixtures/unit/simple.yaml"
     nestedFile = "test/fixtures/unit/nested.yaml"
 
@@ -13,25 +13,25 @@ class TestJsonParser(unittest.TestCase):
         self.assertFalse(YamlFileParser.isFileSupported("/no/path/file"), "no extensions are not supported")
         self.assertFalse(YamlFileParser.isFileSupported("/no/path/file.txt"), "only yaml and yml extensions are not supported")
 
-    # def test_002_simple_file(self) -> None:
-    #     props: dict = JsonFileParser.getVariablesDict(self.simpleFile)
-    #     self.assertEqual(len(props.keys()), 2, "simple file has 2 variables")
-    #     self.assertTrue("major" in props.keys(), "simple contains variable major")
-    #     self.assertTrue("minor" in props.keys(), "simple contains variable minor")
-    #     self.assertEqual(props["major"], "1", "variable major has the expected value")
-    #     self.assertEqual(props["minor"], "2", "variable minor has the expected value")
+        # def test_002_simple_file(self) -> None:
+        props: dict = YamlFileParser.getVariablesDict(self.simpleFile)
+        self.assertEqual(len(props.keys()), 2, "simple file has 2 variables")
+        self.assertTrue("major" in props.keys(), "simple contains variable major")
+        self.assertTrue("minor" in props.keys(), "simple contains variable minor")
+        self.assertEqual(props["major"], "1", "variable major has the expected value")
+        self.assertEqual(props["minor"], "2", "variable minor has the expected value")
 
-    # def test_003_nested_file(self) -> None:
-    #     props: dict = JsonFileParser.getVariablesDict(self.nestedFile)
-    #     self.assertTrue("build_version_minor" in props.keys(), "nested contains variable build_version_minor")
-    #     self.assertTrue("build_version_major" in props.keys(), "nested contains variable build_version_major")
-    #     self.assertEqual(props["build_version_major"], "1", "variable build_version_major has the expected value")
-    #     self.assertEqual(props["build_version_minor"], "2", "variable build_version_minor has the expected value")
+    def test_003_nested_file(self) -> None:
+        props: dict = YamlFileParser.getVariablesDict(self.nestedFile)
+        self.assertTrue("build_version_minor" in props.keys(), "nested contains variable build_version_minor")
+        self.assertTrue("build_version_major" in props.keys(), "nested contains variable build_version_major")
+        self.assertEqual(props["build_version_major"], "1", "variable build_version_major has the expected value")
+        self.assertEqual(props["build_version_minor"], "2", "variable build_version_minor has the expected value")
 
-    # def test_003_nested_file_custom_se(self) -> None:
-    #     props: dict = JsonFileParser.getVariablesDict(self.nestedFile, ".")
-    #     self.assertEqual(len(props.keys()), 2, "nested file has 2 variables")
-    #     self.assertTrue("build.version.minor" in props.keys(), "nested contains variable build.version.minor")
-    #     self.assertTrue("build.version.major" in props.keys(), "nested contains variable build.version.major")
-    #     self.assertEqual(props["build.version.major"], "1", "variable build.version.major has the expected value")
-    #     self.assertEqual(props["build.version.minor"], "2", "variable build.version.minor has the expected value")
+    def test_003_nested_file_custom_se(self) -> None:
+        props: dict = YamlFileParser.getVariablesDict(self.nestedFile, ".")
+        self.assertEqual(len(props.keys()), 2, "nested file has 2 variables")
+        self.assertTrue("build.version.minor" in props.keys(), "nested contains variable build.version.minor")
+        self.assertTrue("build.version.major" in props.keys(), "nested contains variable build.version.major")
+        self.assertEqual(props["build.version.major"], "1", "variable build.version.major has the expected value")
+        self.assertEqual(props["build.version.minor"], "2", "variable build.version.minor has the expected value")
